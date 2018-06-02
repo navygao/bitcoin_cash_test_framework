c4dee9b75c111d494a81617a7338ed601bee13ba
floreslorca <iratxo.floreslorca@gmail.com>
2018-06-02 03:08:43 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bitcoin-cash-test-framework',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='sken',
+    author_email='',
+    description='bitcoin cash test framework package'
+)
